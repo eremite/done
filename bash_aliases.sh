a4fad8065ce6003a040b6cbface24a9b05bf0a5d
Taylor Carpenter <taylor@codecafe.com>
2013-03-05 00:32:53 -0600

    added trailing slash after path

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,6 +1,6 @@
 path=$(dirname `readlink -f ${BASH_SOURCE[0]}`)
 
-alias d="${path}done.thor log"
-alias dg="${path}done.thor gitlog"
-alias db="${path}done.thor browserlog"
-alias dr="${path}done.thor report"
+alias d="${path}/done.thor log"
+alias dg="${path}/done.thor gitlog"
+alias db="${path}/done.thor browserlog"
+alias dr="${path}/done.thor report"
