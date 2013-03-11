7675157f9e90dc747f26370ca2c870747c027f66
Daniel Esplin <daniel@custombit.com>
2013-03-11 10:10:40 -0600

    Fix careless syntax error in bash_aliases.

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,5 +1,5 @@
 readlink_path=$(type -p greadlink readlink | head -1)
-path=$(dirname `$(readlink_path) -f ${BASH_SOURCE[0]}`)
+path=$(dirname `${readlink_path} -f ${BASH_SOURCE[0]}`)
 
 alias d="${path}/done.thor log"
 alias dg="${path}/done.thor gitlog"
